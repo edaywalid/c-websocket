8872b98741c408378ad92b945daae291619db750
edaywalid <becharwalid9@gmail.com>
2025-01-31 12:09:35 +0100

    added some comments for server.c functions

--- server.c
+++ server.c
@@ -3,6 +3,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/*
+ * start_server()
+ *
+ * - Create a new server socket and bind it to the specified port
+ *   and start listening for incoming connections.
+ *   The server socket is returned if successful.
+ *   If an error occurs, the program will exit with EXIT_FAILURE.
+ *   The server socket must be closed with close() when no longer needed.
+ *   The server socket is used to accept incoming client connections.
+ *   The server socket is a TCP socket.
+ *   The server socket is bound to INADDR_ANY to listen on all network
+ * interfaces The server socket is bound to the specified port. The server
+ * socket is set to listen for a maximum of MAX_CLIENTS connections.
+ */
+
 int start_server() {
   int server_fd;
   struct sockaddr_in server_addr;
@@ -33,6 +48,16 @@ int start_server() {
   printf("Server listening on port %d...\n", PORT);
   return server_fd;
 }
+
+/*
+ * accept_client()
+ * - Accept a new client connection on the specified server socket
+ *   and return the client socket file descriptor.
+ *   If an error occurs, the program will exit with EXIT_FAILURE.
+ *
+ *   Parameters:
+ *     - server_fd: The server socket file descriptor
+ */
 
 int accept_client(int server_fd) {
   struct sockaddr_in client_addr;
