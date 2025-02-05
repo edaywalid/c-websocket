f1084fa3aadb74ac13dcb4991ecfa023c614f1fb
edaywalid <becharwalid9@gmail.com>
2025-02-05 14:28:13 +0100

    added websocket.h header file to handle ws handshake

---
+++ websocket.h
@@ -0,0 +1,11 @@
+#ifndef WEBSOCKET_H
+#define WEBSOCKET_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int handle_websocket_handshake(int client_fd);
+
+#endif
